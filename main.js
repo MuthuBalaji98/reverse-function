41965b5540748a309e12bdce2a5cb8cc3be2a103
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:13:09 +0530

    reverse

---
+++ main.js
@@ -0,0 +1,7 @@
+let num=[1,2,3,4,5];
+console.log("original array :", num);
+console.log("length of the array is "+num.length);
+console.log("method : array.reverse()");
+let result=num.reverse();
+console.log("change in array : ", result);
+console.log("return: length of array after reverse() is "+result.length);
